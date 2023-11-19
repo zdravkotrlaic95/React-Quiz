54e9d4174f0700caec7c43bec98a0cfda9c18313
Zdravko Trlaic <zdravkotrlaic95@gmail.com>
2023-11-19 20:59:55 +0100

    add to another branch

--- final/src/components/Question.js
+++ final/src/components/Question.js
@@ -5,6 +5,7 @@ function Question() {
   const { questions, index } = useQuiz();
   const question = questions.at(index);
 
+  console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
